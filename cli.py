ae57dabd96a3bf7609d5ebde74f3b656a6e096d8
Konstantin Dymov <dymov_k_v@ukr.net>
2017-10-03 16:33:16 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,13 +1,10 @@
 import sys
 from core import *
 
-# Run CLI: python cli.py "{a|b}bba" abba bba ababababababa abbabababababbba bbbbbba
-
 args = sys.argv
 regex = sys.argv[1]
 test_cases = sys.argv[2:]
 
 d = FSMBuilder.build_determined(Lexer.tokenize(regex))
-print(d._FSM__states)
 for case in test_cases:
 	print('d acceptance %s %s' % (case, d.acceptance(case)))
