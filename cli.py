85aa07e1d796335170beddecbebe3b799d06d5c6
Konstantin Dymov <dymov_k_v@ukr.net>
2017-10-23 14:40:57 +0300

    Updated cli - added Moore machine building

--- cli.py
+++ cli.py
@@ -3,8 +3,13 @@ from core import *
 
 args = sys.argv
 regex = sys.argv[1]
-test_cases = sys.argv[2:]
+machine_type = int(sys.argv[2])
+test_cases = sys.argv[3:]
 
-d = FSMBuilder.build_determined(Lexer.tokenize(regex))
-for case in test_cases:
-	print('d acceptance %s %s' % (case, d.acceptance(case)))
+if machine_type == 0:
+	d = FSMBuilder.build_determined(Lexer.tokenize(regex))
+	for case in test_cases:
+		print('d acceptance %s %s' % (case, d.acceptance(case)))
+elif machine_type == 1:
+	tokens_lists = [Lexer.tokenize(item.strip()) for item in regex.split(',')]
+	d = MooreMachineBuilder.build_determined(tokens_lists)
