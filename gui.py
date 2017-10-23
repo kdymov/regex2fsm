9159fb442fd45c4bb56b08c1f32d01602cd4d7dd
Konstantin Dymov <dymov_k_v@ukr.net>
2017-10-23 14:41:25 +0300

    Updated gui - added Moore machine building

--- gui.py
+++ gui.py
@@ -1,9 +1,17 @@
 from tkinter import *
 from core import *
 
-def handler(event):
+def build_fsm_handler(event):
 	test_cases = filter(lambda x: len(x) > 0, test_text.get('1.0', 'end').split("\n")) 
 	d = FSMBuilder.build_determined(Lexer.tokenize(regex_entry.get()))
+	result = '\n'.join(['%s %s' % (case, d.acceptance(case)) for case in test_cases])
+	test_text.delete('1.0', 'end')
+	test_text.insert('1.0', result)
+
+def build_moore_handler(event):
+	test_cases = filter(lambda x: len(x) > 0, test_text.get('1.0', 'end').split("\n"))
+	tokens_lists = [Lexer.tokenize(item.strip()) for item in regex_entry.get().split(',')]
+	d = MooreMachineBuilder.build_moore(tokens_lists)
 	result = '\n'.join(['%s %s' % (case, d.acceptance(case)) for case in test_cases])
 	test_text.delete('1.0', 'end')
 	test_text.insert('1.0', result)
@@ -13,14 +21,17 @@ regex_label = Label(root, text="Regular expression")
 regex_entry = Entry(root)
 test_label = Label(root, text="Test cases (one per line)")
 test_text = Text(root, wrap='word')
-run_btn = Button(root, text='Run')
+build_fsm = Button(root, text='Build FSM')
+build_moore = Button(root, text='Build Moore')
 
-run_btn.bind('<Button-1>', handler)
+build_fsm.bind('<Button-1>', build_fsm_handler)
+build_moore.bind('<Button-1>', build_moore_handler)
 
 regex_label.pack()
 regex_entry.pack()
 test_label.pack()
 test_text.pack()
-run_btn.pack()
+build_fsm.pack()
+build_moore.pack()
 
 root.mainloop()
