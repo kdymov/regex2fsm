481f76000bedb5b2d5ec6db8520e3f6f23fc2632
Konstantin Dymov <dymov_k_v@ukr.net>
2017-11-14 17:50:35 +0200

    Added BuchiMachine and BuchiMachineBuilder

--- core.py
+++ core.py
@@ -37,6 +37,10 @@ class IterationToken(Token):
 	def parse(self):
 		return IterationToken(RegexToken(self.content[1:-1]).parse())
 
+class StrongIterationToken(Token):
+	def parse(self):
+		return StrongIterationToken(RegexToken(self.content[1:-1]).parse())
+
 class Lexer:
 	@classmethod
 	def __parse_groups(cls, regex):
@@ -44,14 +48,15 @@ class Lexer:
 		groups = []
 		opened = 0
 		opened_curve = 0
+		opened_bracket = 0
 		for char in regex:
-			if char == '(' and opened_curve == 0:
+			if char == '(' and opened_curve == 0 and opened_bracket == 0:
 				if opened == 0 and len(current_group) > 0:
 					groups.append(SequenceToken(current_group))
 					current_group = ''
 				opened += 1
 				current_group += char
-			elif char == ')' and opened_curve == 0:
+			elif char == ')' and opened_curve == 0 and opened_bracket == 0:
 				opened -= 1
 				if opened < 0:
 					raise ValueError('Incorrect regex')
@@ -59,19 +64,33 @@ class Lexer:
 				if opened == 0:
 					groups.append(GroupToken(current_group))
 					current_group = ''
-			elif char == '{' and opened == 0:
+			elif char == '{' and opened == 0 and opened_bracket == 0:
 				if opened_curve == 0 and len(current_group) > 0:
 					groups.append(SequenceToken(current_group))
 					current_group = ''
 				opened_curve += 1
 				current_group += char
-			elif char == '}' and opened == 0:
+			elif char == '}' and opened == 0 and opened_bracket == 0:
 				opened_curve -= 1
 				if opened_curve < 0:
 					raise ValueError('Incorrect regex')
 				current_group += char
 				if opened_curve == 0:
 					groups.append(IterationToken(current_group))
+					current_group = ''
+			elif char == '[' and opened == 0 and opened_curve == 0:
+				if opened_bracket == 0 and len(current_group) > 0:
+					groups.append(SequenceToken(current_group))
+					current_group = ''
+				opened_bracket += 1
+				current_group += char
+			elif char == ']' and opened == 0 and opened_curve == 0:
+				opened_bracket -= 1
+				if opened_bracket < 0:
+					raise ValueError('Incorrect regex')
+				current_group += char
+				if opened_bracket == 0:
+					groups.append(StrongIterationToken(current_group))
 					current_group = ''
 			else:
 				current_group += char
@@ -600,3 +619,296 @@ class MooreMachineBuilder:
 		dfa = cls.determinize(nfa)
 		dfa.get_dot_structure().render('dfa.gv', view=False)
 		return dfa
+
+class BuchiMachine:
+	EPSILON = '$'
+
+	def __init__(self):
+		self.__states = {}
+		self.__final_states = []
+		self.__current_state = None
+		self.__returns = {}
+
+	def __copy(self):
+		b = BuchiMachine()
+		b.__states = self.__states
+		b.__final_states = self.__final_states
+		b.__current_state = self.__current_state
+		b.__returns = self.__returns
+		return b
+
+	def __transition(self, char):
+		try:
+			ways = self.__states[self.__current_state][char]
+			results = [self.__copy() for _ in ways]
+			for i in range(len(ways)):
+				results[i].__current_state = ways[i]
+			return results
+		except:
+			return []
+
+	def __is_in_final_state(self):
+		return self.__current_state in self.__final_states
+
+	def all_possible_transitions(self, states, char):
+		result = []
+		for state in states:
+			try:
+				transitions = self.__states[state][char]
+				for t in transitions:
+					result += self.epsilon_closure(t)
+			except:
+				pass
+		result = sorted(list(set(result)))
+		return result
+
+	def all_possible_chars(self, states):
+		result = []
+		for state in states:
+			result += self.__states[state].keys()
+		result = sorted(list(set(result)))
+		return result
+
+	def add_state(self, index, return_list, is_final):
+		if index in self.__states:
+			raise ValueError('State is already exist in BuchiMachine')
+		else:
+			self.__states[index] = {}
+			self.__returns[index] = return_list
+			if is_final:
+				self.__final_states.append(index)
+
+	def add_transition(self, source, target, char):
+		if source in self.__states and target in self.__states:
+			try:
+				self.__states[source][char].append(target)
+			except:
+				self.__states[source][char] = [target]
+		else:
+			raise ValueError('Source or target does not exist in BuchiMachine')
+
+	def set_initial_state(self, index):
+		if index in self.__states:
+			self.__current_state = index
+		else:
+			raise ValueError('State does not exist in BuchiMachine')
+
+	def add_final_state(self, index):
+		if index in self.__states:
+			self.__final_states.append(index)
+		else:
+			raise ValueError('State does not exist in BuchiMachine')
+
+	def merge_states(self, from_state, to_state):
+		print('MERGE FROM %s TO %s' % (from_state, to_state))
+		for state in self.__states:
+			for char in self.__states[state]:
+				if from_state in self.__states[state][char]:
+					if to_state in self.__states[state][char]:
+						self.__states[state][char].remove(from_state)
+					else:
+						self.__states[state][char][self.__states[state][char].index(from_state)] = to_state
+		for char in self.__states[from_state]:
+			if char in self.__states[to_state]:
+				self.__states[to_state][char] += self.__states[from_state][char]
+				self.__states[to_state][char] = list(set(self.__states[to_state][char]))
+			else:
+				self.__states[to_state][char] = self.__states[from_state][char]
+		if from_state in self.__final_states:
+			if not to_state in self.__final_states:
+				self.add_final_state(to_state)
+			self.__final_states.remove(from_state)
+		del self.__states[from_state]
+
+	def epsilon_closure(self, index):
+		result = [index]
+		try:
+			epsilon_ways = self.__states[index][BuchiMachine.EPSILON]
+			for way in epsilon_ways:
+				result += self.epsilon_closure(way)
+		except:
+			pass
+		return sorted(list(set(result)))
+
+	def all_epsilon_closures(self):
+		result = []
+		for index in self.__states:
+			result.append((index, self.epsilon_closure(index)))
+		return result
+
+	def acceptance(self, s):
+		prev_state = self.__current_state
+		if len(s) == 0:
+			result = self.__returns[self.__current_state]
+			self.__current_state = prev_state
+			return result
+		else:
+			new_start = self.__transition(s[0])
+			print(new_start)
+			if len(new_start) == 0:
+				self.__current_state = prev_state
+				return 'None'
+			else:
+				self.set_initial_state(new_start[0].__current_state)
+				result = self.acceptance(s[1:])
+				self.__current_state = prev_state
+				return result
+
+	def get_dot_structure(self):
+		dot = Digraph()
+		dot.format = 'png'
+		dot.attr(rankdir='LR')
+		dot.node('', shape='none')
+		for state in self.__states:
+			if state in self.__final_states:
+				dot.node(state, shape='doublecircle')
+			else:
+				dot.node(state, shape='circle')
+		dot.edge('', self.__current_state)
+		for state in self.__states:
+			for char in self.__states[state]:
+				for transition in self.__states[state][char]:
+					dot.edge(state, transition, label=char)
+		return dot
+
+class BuchiMachineBuilder:
+	@classmethod
+	def build(cls, tokens_lists):
+		a = BuchiMachine()
+		a.add_state('0', None, False)
+		last_state = 0
+		addr = []
+		a.set_initial_state('0')
+		for tokens in tokens_lists:
+			last_state += 1
+			a.add_state(str(last_state), 'R' + str(last_state), True)
+			a.add_transition('0', str(last_state), '@' + str(last_state - 1))
+			addr.append(('@' + str(last_state - 1), tokens, '0', str(last_state)))
+		address = len(addr)
+		while len(addr) > 0:
+			current = addr[0]
+			if type(current[1]) == type([]):
+				prev = current[2]
+				last_in_seq = current[3]
+				del a._BuchiMachine__states[current[2]][current[0]]
+				for i in current[1][:-1]:
+					last_state += 1
+					a.add_state(str(last_state), None, False)
+					if type(i) == type(LetterToken('')):
+						a.add_transition(str(prev), str(last_state), i.content)
+					elif type(i) == type(GroupToken('')):
+						addr.append(('@' + str(address), i.content, str(prev), str(last_state)))
+						a.add_transition(str(prev), str(last_state), '@' + str(address))
+						address += 1
+					else:
+						addr.append(('@' + str(address), i, str(prev), str(last_state)))
+						a.add_transition(str(prev), str(last_state), '@' + str(address))
+						address += 1
+					prev = last_state
+				if type(current[1][-1]) == type(LetterToken('')):
+					a.add_transition(str(prev), str(last_in_seq), current[1][-1].content)
+				elif type(current[1][-1]) == type(GroupToken('')):
+					addr.append(('@' + str(address), current[1][-1].content, str(prev), str(last_in_seq)))
+					a.add_transition(str(prev), str(last_in_seq), '@' + str(address))
+					address += 1
+				else:
+					addr.append(('@' + str(address), current[1][-1], str(prev), str(last_in_seq)))
+					a.add_transition(str(prev), str(last_in_seq), '@' + str(address))
+					address += 1
+				addr = addr[1:]
+			elif type(current[1]) == type(DisjunctionToken('')):
+				del a._BuchiMachine__states[current[2]][current[0]]
+				from_state = current[2]
+				to_state = current[3]
+				for item in current[1].content:
+					addr.append(('@' + str(address), item, from_state, to_state))
+					a.add_transition(from_state, to_state, '@' + str(address))
+					address += 1
+				addr = addr[1:]
+			elif type(current[1]) == type(IterationToken('')):
+				del a._BuchiMachine__states[current[2]][current[0]]
+				from_state = current[2]
+				to_state = current[3]
+				last_state += 1
+				a.add_state(str(last_state), None, False)
+				a.add_transition(from_state, str(last_state), BuchiMachine.EPSILON)
+				a.add_transition(str(last_state), to_state, BuchiMachine.EPSILON)
+				addr.append(('@' + str(address), current[1].content, str(last_state), str(last_state)))
+				a.add_transition(str(last_state), str(last_state), '@' + str(address))
+				address += 1
+				addr = addr[1:]
+			elif type(current[1]) == type(StrongIterationToken('')):
+				del a._BuchiMachine__states[current[2]][current[0]]
+				from_state = current[2]
+				to_state = current[3]
+				a.add_transition(from_state, to_state, '@' + str(address))
+				addr.append(('@' + str(address), current[1].content, from_state, from_state))
+				a.merge_states(to_state=from_state, from_state=to_state)
+				address += 1
+				addr = addr[1:]
+			else:
+				break
+		return a
+
+	@classmethod
+	def determinize(cls, buchi_machine):
+		def find_key_by_value(d, v):
+			for k in d:
+				if d[k] == v:
+					return k
+			return None
+
+		determined = BuchiMachine()
+		start = buchi_machine.epsilon_closure(buchi_machine._BuchiMachine__current_state)
+		new_keys = { '0': start }
+		queue = ['0']
+		new_state_key = 1
+		is_final = False
+		mark = None
+		for item in start:
+			if item in buchi_machine._BuchiMachine__final_states:
+				is_final = True
+				if mark is None:
+					mark = [buchi_machine._BuchiMachine__returns[item]]
+				else:
+					mark.append(buchi_machine._BuchiMachine__returns[item])
+		determined.add_state(queue[0], mark, is_final)
+		determined.set_initial_state(queue[0])
+		while len(queue) > 0:
+			current = queue[0]
+			queue = queue[1:]
+			closure = new_keys[current]
+			chars = buchi_machine.all_possible_chars(closure)
+			if BuchiMachine.EPSILON in chars:
+				pos = chars.index(BuchiMachine.EPSILON)
+				chars = chars[:pos] + chars[pos + 1:]
+			for char in chars:
+				trans = buchi_machine.all_possible_transitions(closure, char)
+				to_state = find_key_by_value(new_keys, trans)
+				if to_state is None:
+					to_state = str(new_state_key)
+					new_state_key += 1
+					new_keys[to_state] = trans
+					queue.append(to_state)
+					is_final = False
+					mark = None
+					for item in trans:
+						if item in buchi_machine._BuchiMachine__final_states:
+							is_final = True
+							if mark is None:
+								mark = [buchi_machine._BuchiMachine__returns[item]]
+							else:
+								mark.append(buchi_machine._BuchiMachine__returns[item])
+					determined.add_state(to_state, mark, is_final)
+				determined.add_transition(current, to_state, char)
+		return determined
+
+	@classmethod
+	def build_buchi(cls, tokens_lists):
+		nfa = cls.build(tokens_lists)
+		print(nfa._BuchiMachine__states)
+		nfa.get_dot_structure().render('nfa.gv', view=False)
+		dfa = cls.determinize(nfa)
+		dfa.get_dot_structure().render('dfa.gv', view=False)
+		return dfa
+
